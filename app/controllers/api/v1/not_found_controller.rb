03f1e7767fd45b728d71b3f6ed4498e4a9317356
DanielAmah <amahdanieljack@gmail.com>
2017-12-30 11:05:51 +0100

    setup api versioning

--- app/controllers/api/v1/not_found_controller.rb
+++ app/controllers/api/v1/not_found_controller.rb
@@ -1,7 +1,11 @@
+module Api
+  module V1
 class NotFoundController < ApplicationController
   def index
     status = 404
     obj = { message: 'where you are going does not exist' }
     json_response(obj, status)
   end
+end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,52 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :categories  do  
-    resources :products do
-      resources :order_items
-    end
-  end
-  
-  resources :order_status_codes do
-    resources :orders do
-      resources :shipments
-      resources :invoices do
-        resources :payments
+  namespace :api, defaults: {format: 'json'} do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :categories  do  
+        resources :products do
+          resources :order_items
+        end
       end
-      resources :order_items do
-        resources :shipment_items
+      
+      resources :order_status_codes do
+        resources :orders do
+          resources :shipments
+          resources :invoices do
+            resources :payments
+          end
+          resources :order_items do
+            resources :shipment_items
+          end
+        end
       end
-    end
-  end
 
-  resources :order_item_status_codes do
-    resources :order_items
-  end
-
-  resources :invoice_status_codes do
-    resources :invoices do
-      resources :shipments do
-        resources :shipment_items
+      resources :order_item_status_codes do
+        resources :order_items
       end
-    end
-  end
 
-  resources :payment_methods
+      resources :invoice_status_codes do
+        resources :invoices do
+          resources :shipments do
+            resources :shipment_items
+          end
+        end
+      end
 
-  resources :roles
-  resources :users do
-    collection do 
-      get 'search/:q', :action => 'search', :as => 'search'
+      resources :payment_methods
+
+      resources :roles
+      resources :users do
+        collection do 
+          get 'search/:q', :action => 'search', :as => 'search'
+        end
+      end
+      get 'test', to: 'users#test'
+      match '/(*url)', to: 'not_found#index', via: :all
     end
   end
 
   post 'auth/register', to: 'users#register'
   post 'auth/login', to: 'users#login'
-  get 'test', to: 'users#test'
-  
-
-  match '/(*url)', to: 'not_found#index', via: :all
 end
